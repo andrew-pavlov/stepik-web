7775432e13f6fe097da62a930dbb2be1dc9beb96
andrew-pavlov <pavlov.andrew.m@gmail.com>
2018-02-28 19:28:12 +0000

    django test 5 error

--- ask/ask.sh
+++ ask/ask.sh
@@ -1,4 +1,2 @@
 pkill gunicorn
-gunicorn -b 0.0.0.0:8080 hello:app &
-cd ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi &
--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
-sudo rm /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-
 sudo /etc/init.d/nginx restart
